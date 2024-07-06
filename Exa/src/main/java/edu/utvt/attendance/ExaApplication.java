edf671dd8876f9fd6b30949b68cd6e239dd23797
Hugo Sanchez Garcia <al222010010@gmail.com>
2024-07-06 11:14:55 -0600

    Initial commit

---
+++ Exa/src/main/java/edu/utvt/attendance/ExaApplication.java
@@ -0,0 +1,13 @@
+package edu.utvt.attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExaApplication.class, args);
+	}
+
+}
